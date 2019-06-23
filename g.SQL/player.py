5da2c0fb540c4a59f1de9f418c2db474c38c56bb
Rubén Crespo-Cano <rcrespocano@gmail.com>
2019-06-23 15:08:02 +0200

    New notebook: SQL (SQLite)

---
+++ g.SQL/player.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+class Player:
+    """A sample Player class"""
+    
+    def __init__(self, first, last, height, weight, age):
+        self.first = first
+        self.last = last
+        self.height = height
+        self.weight = weight
+        self.age = age
+
+    
